bebe58b86baf61cf189e1a4ba6accaa6b6a734fc
Ahmed Faraz Ansari <af.ahmfrz@gmail.com>
2018-08-19 16:18:01 +0530

    update(auth)
    update auth-helper to include exception handling
    update auth routes to remove 'failureFlash' option

--- auth/auth-helper.js
+++ auth/auth-helper.js
@@ -4,54 +4,58 @@ var User = require('../models/user.model');
 var Membership = require('../models/membership.model');
 
 module.exports.updateMembership = function(accessToken, refreshToken, profile, done, config) {
-    Membership.findOne({ providerUserId: profile.id }, function(err, membership) {
-        if (err) {
-            logger.error("Could not find membership for ", profile.id, "error ", err);
-            return done(err);
-        }
+    try {
+        Membership.findOne({ providerUserId: profile.id }, function(err, membership) {
+            if (err) {
+                logger.error("Could not find membership for ", profile.id, "error ", err);
+                return done(err);
+            }
 
-        if (!membership) {
-            var user = new User({
-                username: profile.id,
-                displayname: profile.displayName,
-            });
-
-            user.save(function(err) {
-                if (err) {
-                    logger.error("Error creating a new user for ", profile.id, "error ", err);
-                    return done(err);
-                }
-
-                membership = new Membership({
-                    provider: profile.provider,
-                    providerUserId: profile.id,
-                    accessToken: accessToken,
-                    userId: user
+            if (!membership) {
+                var user = new User({
+                    username: profile.id,
+                    displayname: profile.displayName,
                 });
 
-                membership.save(function(err) {
+                user.save(function(err) {
                     if (err) {
-                        logger.error("Error creating a new membership for ", profile.id, "error ", err);
+                        logger.error("Error creating a new user for ", profile.id, "error ", err);
                         return done(err);
                     }
-                    return done(null, jwt.sign({ username: user.username }, config.SECRET, {
+
+                    membership = new Membership({
+                        provider: profile.provider,
+                        providerUserId: profile.id,
+                        accessToken: accessToken,
+                        userId: user
+                    });
+
+                    membership.save(function(err) {
+                        if (err) {
+                            logger.error("Error creating a new membership for ", profile.id, "error ", err);
+                            return done(err);
+                        }
+                        return done(null, jwt.sign({ username: user.username }, config.SECRET, {
+                            expiresIn: '24h', // expires in 24 hours
+                        }));
+                    });
+                });
+            } else {
+                User.findById(membership.userId, function(err, user) {
+                    if (err) {
+                        logger.error("Error finding user ", membership.userId, "error ", err);
+                        return done(err);
+                    }
+
+                    var token = jwt.sign({ username: user.username }, config.SECRET, {
                         expiresIn: '24h', // expires in 24 hours
-                    }));
-                });
-            });
-        } else {
-            User.findById(membership.userId, function(err, user) {
-                if (err) {
-                    logger.error("Error finding user ", membership.userId, "error ", err);
-                    return done(err);
-                }
+                    });
 
-                var token = jwt.sign({ username: user.username }, config.SECRET, {
-                    expiresIn: '24h', // expires in 24 hours
+                    return done(null, token);
                 });
-
-                return done(null, token);
-            });
-        }
-    });
+            }
+        });
+    } catch (err) {
+        return done(err);
+    }
 }
--- routes/auth.js
+++ routes/auth.js
@@ -3,14 +3,14 @@ var router = express.Router();
 var passport = require('passport');
 
 router.get('/api/test',
-    passport.authenticate('bearer', { session: false, failureFlash: true }),
+    passport.authenticate('bearer', { session: false }),
     function(req, res) {
         res.json(req.user);
     });
 
 router.post(
     '/login',
-    passport.authenticate('local', { session: false, failureFlash: true }),
+    passport.authenticate('local', { session: false }),
     function(req, res) {
         res.json({
             token: req
@@ -21,7 +21,7 @@ router.post(
 // Redirect the user to the OAuth 2.0 provider for authentication.  When
 // complete, the provider will redirect the user back to the application at
 //     /auth/provider/callback
-router.get('/provider', passport.authenticate('provider', { scope: 'email', failureFlash: true }));
+router.get('/provider', passport.authenticate('provider', { scope: 'email' }));
 
 // The OAuth 2.0 provider has redirected the user back to the application.
 // Finish the authentication process by attempting to obtain an access
@@ -31,12 +31,11 @@ router.get('/provider/callback',
     passport.authenticate('provider', {
         successRedirect: '/',
         failureRedirect: '/login',
-        failureFlash: true
     }));
 
 // Redirect the user to Facebook for authentication.  When complete,
 // Facebook will redirect the user back to the application at
-router.get('/facebook', passport.authenticate('facebook', { session: false, failureFlash: true }));
+router.get('/facebook', passport.authenticate('facebook', { session: false }));
 
 // Facebook will redirect the user to this URL after approval.  Finish the
 // authentication process by attempting to obtain an access token.  If
@@ -47,7 +46,6 @@ router.get('/facebook/callback',
         successReturnToOrRedirect: '/',
         failureRedirect: '/auth/setup/',
         session: false,
-        failureFlash: true
     })
 );
 
@@ -60,7 +58,6 @@ router.get('/google',
     passport.authenticate('google', {
         scope: ['profile'],
         session: false,
-        failureFlash: true
     }));
 
 // GET /auth/google/callback
@@ -73,7 +70,6 @@ router.get('/google/callback',
         successReturnToOrRedirect: '/',
         failureRedirect: '/auth/setup/',
         session: false,
-        failureFlash: true
     }));
 
 module.exports = router;
